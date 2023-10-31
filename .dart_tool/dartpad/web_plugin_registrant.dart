6e17668296f654d64b7a791d3a749e6faa65b849
chase garsee <chasegarsee@gmail.com>
2023-10-31 08:04:12 -0500

    update files to match xcode / add correct build number

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,32 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audio_session/audio_session_web.dart';
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:just_audio_web/just_audio_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+import 'package:wakelock_web/wakelock_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioSessionWeb.registerWith(registrar);
+  FirebaseFirestoreWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
+  JustAudioPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  WakelockWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
