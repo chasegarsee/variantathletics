2ffe5bfd4bda5b2b5e67403ec8b0fa7d2fb90fba
chase garsee <chasegarsee@gmail.com>
2023-11-15 20:25:12 +0700

    update info.plist

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -8,10 +8,13 @@
 
 import 'package:audio_session/audio_session_web.dart';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:just_audio_web/just_audio_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:video_player_web/video_player_web.dart';
 import 'package:wakelock_web/wakelock_web.dart';
@@ -21,10 +24,13 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   AudioSessionWeb.registerWith(registrar);
   FirebaseFirestoreWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FlutterNativeSplashWeb.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
   JustAudioPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
+  SignInWithApplePlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   VideoPlayerPlugin.registerWith(registrar);
   WakelockWeb.registerWith(registrar);
