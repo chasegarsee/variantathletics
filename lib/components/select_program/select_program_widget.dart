066ae16c86a58b28ff83fe8046273da8fd3eed91
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-30 17:14:42 +0000

    Updating to latest FlutterFlow output.

--- lib/components/select_program/select_program_widget.dart
+++ lib/components/select_program/select_program_widget.dart
@@ -2,7 +2,6 @@ import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/backend/schema/structs/index.dart';
 import '/components/no_comments_to_display/no_comments_to_display_widget.dart';
-import '/components/program_countdown/program_countdown_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'dart:ui';
@@ -453,13 +452,6 @@ class _SelectProgramWidgetState extends State<SelectProgramWidget> {
                                                     FlutterFlowTheme.of(context)
                                                         .primaryText,
                                               ),
-                                        ),
-                                        ProgramCountdownWidget(
-                                          key: Key(
-                                              'Keya68_${listViewIndex}_of_${listViewProgramsRecordList.length}'),
-                                          currentTime: getCurrentTimestamp,
-                                          liveDate:
-                                              listViewProgramsRecord.liveDate!,
                                         ),
                                       ],
                                     );
