d652fff8dee8ae276b11fc537471d1dde753796d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-17 03:10:39 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/nutritionhub/nutritionhub_widget.dart
+++ lib/pages/nutritionhub/nutritionhub_widget.dart
@@ -5,6 +5,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import '/flutter_flow/custom_functions.dart' as functions;
+import '/flutter_flow/random_data_util.dart' as random_data;
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -83,11 +84,7 @@ class _NutritionhubWidgetState extends State<NutritionhubWidget> {
     }
 
     context.watch<FFAppState>();
-    final chartPieChartColorsList = [
-      FlutterFlowTheme.of(context).error,
-      FlutterFlowTheme.of(context).warning,
-      FlutterFlowTheme.of(context).accent2
-    ];
+
     return GestureDetector(
       onTap: () => _model.unfocusNode.canRequestFocus
           ? FocusScope.of(context).requestFocus(_model.unfocusNode)
@@ -483,31 +480,47 @@ class _NutritionhubWidgetState extends State<NutritionhubWidget> {
                 ],
               ),
               Row(
-                mainAxisSize: MainAxisSize.max,
+                mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    width: 371.0,
+                    width: 214.0,
                     height: 175.0,
                     child: FlutterFlowPieChart(
                       data: FFPieChartData(
-                        values: functions.macroPercentageCalc(
-                            FFAppState().macros.totalCalories,
-                            FFAppState().macros.carbs,
-                            FFAppState().macros.protein,
-                            FFAppState().macros.fat),
-                        colors: chartPieChartColorsList,
-                        radius: [50.0],
-                        borderColor: [FlutterFlowTheme.of(context).primaryText],
+                        values: [random_data.randomInteger(0, 10)],
+                        colors: [FlutterFlowTheme.of(context).primary],
+                        radius: [70.0],
                       ),
-                      donutHoleRadius: 30.0,
+                      donutHoleRadius: 0.0,
                       donutHoleColor: Colors.transparent,
-                      sectionLabelType: PieChartSectionLabelType.value,
                       sectionLabelStyle:
                           FlutterFlowTheme.of(context).headlineSmall,
                     ),
                   ),
                 ],
+              ),
+              Builder(
+                builder: (context) {
+                  final percentages = functions
+                      .macroPercentageCalc(
+                          FFAppState().macros.totalCalories,
+                          FFAppState().macros.carbs,
+                          FFAppState().macros.protein,
+                          FFAppState().macros.fat)
+                      .toList();
+                  return Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children:
+                        List.generate(percentages.length, (percentagesIndex) {
+                      final percentagesItem = percentages[percentagesIndex];
+                      return Text(
+                        percentagesItem.toString(),
+                        style: FlutterFlowTheme.of(context).bodyMedium,
+                      );
+                    }),
+                  );
+                },
               ),
             ],
           ),
