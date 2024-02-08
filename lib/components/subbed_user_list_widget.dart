b406c443db13a548c6a644e8427449fe6bf5a6ac
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-08 16:51:45 +0000

    wip

--- lib/components/subbed_user_list_widget.dart
+++ lib/components/subbed_user_list_widget.dart
@@ -58,53 +58,58 @@ class _SubbedUserListWidgetState extends State<SubbedUserListWidget> {
   Widget build(BuildContext context) {
     context.watch<FFAppState>();
 
-    return FlutterFlowDropDown<String>(
-      multiSelectController: _model.dropDownValueController ??=
-          FormFieldController<List<String>>(
-              _model.dropDownValue ??= List<String>.from(
-        functions.stringListNullSafetyCheck(widget.programUserIds!.toList()),
-      )),
-      options: List<String>.from(
-          functions.stringListNullSafetyCheck(widget.userIds!.toList())),
-      optionLabels:
-          functions.stringListNullSafetyCheck(widget.displayNames!.toList()),
-      width: 300.0,
-      height: 50.0,
-      maxHeight: 250.0,
-      searchHintTextStyle: FlutterFlowTheme.of(context).labelMedium,
-      searchTextStyle: FlutterFlowTheme.of(context).bodyMedium,
-      textStyle: FlutterFlowTheme.of(context).bodyMedium,
-      hintText: FFLocalizations.of(context).getText(
-        'udiawp38' /* Please select... */,
+    return Visibility(
+      visible: functions.nullCheck(widget.displayNames?.toList()) ||
+          functions.nullCheck(widget.userIds?.toList()) ||
+          functions.nullCheck(widget.programUserIds?.toList()),
+      child: FlutterFlowDropDown<String>(
+        multiSelectController: _model.dropDownValueController ??=
+            FormFieldController<List<String>>(
+                _model.dropDownValue ??= List<String>.from(
+          functions.stringListNullSafetyCheck(widget.programUserIds!.toList()),
+        )),
+        options: List<String>.from(
+            functions.stringListNullSafetyCheck(widget.userIds!.toList())),
+        optionLabels:
+            functions.stringListNullSafetyCheck(widget.displayNames!.toList()),
+        width: 300.0,
+        height: 50.0,
+        maxHeight: 250.0,
+        searchHintTextStyle: FlutterFlowTheme.of(context).labelMedium,
+        searchTextStyle: FlutterFlowTheme.of(context).bodyMedium,
+        textStyle: FlutterFlowTheme.of(context).bodyMedium,
+        hintText: FFLocalizations.of(context).getText(
+          'udiawp38' /* Please select... */,
+        ),
+        searchHintText: FFLocalizations.of(context).getText(
+          'my0caze8' /* Search for an item... */,
+        ),
+        icon: Icon(
+          Icons.keyboard_arrow_down_rounded,
+          color: FlutterFlowTheme.of(context).secondaryText,
+          size: 24.0,
+        ),
+        fillColor: FlutterFlowTheme.of(context).secondaryBackground,
+        elevation: 2.0,
+        borderColor: FlutterFlowTheme.of(context).alternate,
+        borderWidth: 2.0,
+        borderRadius: 8.0,
+        margin: EdgeInsetsDirectional.fromSTEB(16.0, 4.0, 16.0, 4.0),
+        hidesUnderline: true,
+        isOverButton: true,
+        isSearchable: true,
+        isMultiSelect: true,
+        onMultiSelectChanged: (val) async {
+          setState(() => _model.dropDownValue = val);
+          await widget.programId!.update({
+            ...mapToFirestore(
+              {
+                'clientIds': _model.dropDownValue,
+              },
+            ),
+          });
+        },
       ),
-      searchHintText: FFLocalizations.of(context).getText(
-        'my0caze8' /* Search for an item... */,
-      ),
-      icon: Icon(
-        Icons.keyboard_arrow_down_rounded,
-        color: FlutterFlowTheme.of(context).secondaryText,
-        size: 24.0,
-      ),
-      fillColor: FlutterFlowTheme.of(context).secondaryBackground,
-      elevation: 2.0,
-      borderColor: FlutterFlowTheme.of(context).alternate,
-      borderWidth: 2.0,
-      borderRadius: 8.0,
-      margin: EdgeInsetsDirectional.fromSTEB(16.0, 4.0, 16.0, 4.0),
-      hidesUnderline: true,
-      isOverButton: true,
-      isSearchable: true,
-      isMultiSelect: true,
-      onMultiSelectChanged: (val) async {
-        setState(() => _model.dropDownValue = val);
-        await widget.programId!.update({
-          ...mapToFirestore(
-            {
-              'clientIds': _model.dropDownValue,
-            },
-          ),
-        });
-      },
     );
   }
 }
